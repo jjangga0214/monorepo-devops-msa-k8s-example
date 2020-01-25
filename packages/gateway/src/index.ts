2930291776dc400131ce10ade7c882d29a2ec3c4
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-01-25 22:25:38 +0900

    feat(gateway): add jwt and service secret verficiation
    An authenticated user sends jwt token. The gateway verifies it. If
    verified, it attaches 'X-User-Id' and 'X-User-Role' headers to services.
    A service does send jwt to gateway. Services uses 'X-Service-Secret'
    header to prove their identity. If it's valid, gateway respects
    'X-User-Id' and 'X-User-Role' as-is, and convey them to other services.

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,13 +1,14 @@
 import { ApolloServer } from 'apollo-server'
-import { ApolloGateway } from '@apollo/gateway'
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway'
+import { verify } from 'jsonwebtoken'
 
-// class AuthenticatedDataSource extends RemoteGraphQLDataSource {
-//   willSendRequest({ request, context }) {
-//     if (context.user) {
-//       request.http.headers.set('X-User-Id', context.user.id)
-//     }
-//   }
-// }
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  // eslint-disable-next-line class-methods-use-this
+  willSendRequest({ request, context }) {
+    request.http.headers.set('X-User-Id', context.user.id)
+    request.http.headers.set('X-User-Role', context.user.role)
+  }
+}
 
 // Initialize an ApolloGateway instance and pass it an array of implementing
 // service names and URLs
@@ -17,12 +18,43 @@ const gateway = new ApolloGateway({
     { name: 'auth', url: process.env.AUTH_ENDPOINT },
     // more services
   ],
+  buildService({ url }) {
+    return new AuthenticatedDataSource({ url })
+  },
 })
 
 async function main() {
   const { schema, executor } = await gateway.load()
 
-  const server = new ApolloServer({ schema, executor, subscriptions: false })
+  const server = new ApolloServer({
+    schema,
+    executor,
+    context: ({ req }) => {
+      const isFromService =
+        req.headers['X-Service-Secret'] === process.env.SERVICE_SECRET
+
+      if (isFromService) {
+        return {
+          req,
+          isFromService,
+          user: {
+            id: req.headers['X-User-Id'],
+            role: req.headers['X-User-Role'],
+          },
+        }
+      }
+      const token = req.headers.authorization
+        ? req.headers.authorization.replace('Bearer ', '').trim()
+        : null
+      const { user } = verify(token, process.env.AUTH_SECRET)
+      return {
+        req,
+        isFromService,
+        user,
+      }
+    },
+    subscriptions: false,
+  })
 
   server
     .listen({
