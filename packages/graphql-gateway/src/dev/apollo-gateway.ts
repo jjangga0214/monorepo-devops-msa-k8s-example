7ef393dbc5a3b072e198a6f05b647cd54b7df8ff
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-01-31 00:55:39 +0900

    refactor(graphql-gateway): create DevApolloServer
    This will enable remote service schema polling and remove repeatitive logs while doing so.

---
+++ packages/graphql-gateway/src/dev/apollo-gateway.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable import/no-extraneous-dependencies */
+import { ApolloGateway, GatewayConfig } from '@apollo/gateway'
+import loglevel, { Logger } from 'loglevel'
+import loglevelDebug from 'loglevel-debug'
+import delay from 'delay'
+
+export class DevApolloGateway extends ApolloGateway {
+  public get logger_() {
+    return this.logger
+  }
+
+  protected customLogger: Logger
+
+  constructor(config?: GatewayConfig) {
+    super(config)
+
+    // Setup logging facilities, scoped under the appropriate name.
+    this.customLogger = loglevel.getLogger(`apollo-gateway:`)
+
+    // Support DEBUG environment variable, à la https://npm.im/debug/.
+    loglevelDebug(this.customLogger)
+
+    // And also support the `debug` option, if it's truthy.
+    if (this.config.debug === true) {
+      this.customLogger.enableAll()
+    }
+
+    this.logger_.info = arg => {
+      if (!arg.includes('Gateway successfully loaded schema')) {
+        this.customLogger.info(arg)
+      }
+    }
+    const debugLogBlackList = [
+      'No change in service definitions since last check',
+      'Loading configuration for gateway',
+    ]
+    this.logger_.debug = arg => {
+      if (
+        debugLogBlackList
+          .map(sentence => !arg.includes(sentence))
+          .reduce((previous, current) => previous && current)
+      ) {
+        console.log(arg)
+      }
+    }
+
+    const pollSchemas = async () => {
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        await delay(500)
+        this.load()
+      }
+    }
+    pollSchemas()
+  }
+}
