2bd4ae250a381cc13fc93bf85674d0af14dc3c08
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-01-31 19:37:59 +0900

    refactor(stitching-gateway): remove custom types for node-fetch in favor of cross-fetch

--- packages/stitching-gateway/@types/node-fetch/node-fetch.d.ts
+++
@@ -1,5 +0,0 @@
-// real node-fetch types clash with apollo-link-http, so manually define it as globalfetch here.
-declare module 'node-fetch' {
-  const fetch: GlobalFetch['fetch']
-  export default fetch
-}
