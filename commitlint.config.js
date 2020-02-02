fb2d22983ef5ac56f0ad83379d88553dec52944f
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-02-02 16:32:00 +0900

    build(commitlint): update subject-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [0, 'always', 100], // corresponding to maxHeaderWidth of commitizen
+    'subject-case': [1, 'always', 'lowerCase'],
   },
 }
