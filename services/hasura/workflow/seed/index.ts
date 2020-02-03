643c0c858247ad6071872d6b453f0812d83cc8dd
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-02-03 17:27:24 +0900

    chore(hasura): add faker for seed to reproduce same id of records

--- services/hasura/workflow/seed/index.ts
+++ services/hasura/workflow/seed/index.ts
@@ -1,17 +1,22 @@
 import { requestToHasura } from '@jjangga0214/communication'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { hash } from 'bcryptjs'
+import faker from 'faker'
 import { CREATE_USER } from './mutations'
+
+faker.seed(123) // This ensures data's sequence.
 
 async function main() {
   await requestToHasura(CREATE_USER, {
     objects: [
       {
+        id: faker.random.uuid(),
         username: 'hello',
         password: await hash('foo', 10),
         role: 'ADMIN',
       },
       {
+        id: faker.random.uuid(),
         username: 'hello2',
         password: await hash('bar', 10),
         role: 'USER',
